3663d9d0829493543d425488f52662fd170bec5c
Leif Ashley <leifashley@gmail.com>
2022-03-15 02:37:19 -0400

    Launch screen delay update

--- Batch 5 Demo/App Support/SceneDelegate.swift
+++ Batch 5 Demo/App Support/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
         window?.makeKeyAndVisible()
 
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
             self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
         }
 
